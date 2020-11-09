1df08927bbff1d2731479d1ddf59e68bb11bdfdc
sheng <18475140601@163.com>
2020-11-09 17:04:02 +0800

    数组去重

--- 数组去重/01_利用ES6 Set去重.js
+++ 数组去重/01_利用ES6 Set去重.js
@@ -7,8 +7,6 @@ let arr = [
 // 可以去掉相同的 NaN，不会去掉看起来一样的 对象
 let arr2 = Array.from(new Set(arr));
 
-// 不考虑兼容性，这种去重的方法代码最少。这种方法还无法去掉“{}”空对象，后面的高阶方法会添加去掉重复“{}”的方法
-
 console.log(arr2); // [1, 'true', true, 15, false, undefined, null, NaN, 'NaN', 0, 'a', {}, {}]
 
 console.log([...new Set(arr)]); // 简写方法
